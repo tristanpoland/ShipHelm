f8e56edb969dbe063ff04829ccb07c0c76a468a5
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2023-03-20 12:55:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
