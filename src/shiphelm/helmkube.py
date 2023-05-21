ff0748e7844b21d728a5ec80e0da9462789ff063
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2023-05-21 10:59:52 -0400

    Patched import statement

--- src/shiphelm/helmkube.py
+++ src/shiphelm/helmkube.py
@@ -14,7 +14,8 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------
 
-from kubernetes import client, config, json
+from kubernetes import client, config
+import json
 
 class helmkube:
     def __init__(self, remote_address = None, remote_is_TLS = None, namespace="default"):
